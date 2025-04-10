a2bf752c6476c28e4de31443c8ee9ae362fcb328
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-04-10 06:31:49 +0000

    compare

---
+++ 1_Basic_js/4_comparison.js
@@ -0,0 +1,35 @@
+// Number Comparisons:
+
+// console.log(2 > 1);   //ture
+// console.log(2 >= 1);  //ture
+// console.log(2 < 1);   //false
+// console.log(2 == 1);  //false
+// console.log(2 != 1);  //ture
+
+
+// String Comparisons:
+
+// console.log("2" > 1);  //true
+// console.log("02" > 1); //ture
+
+
+
+// Comparisons with null:
+
+// console.log(null > 0); // => false
+// console.log(null == 0); // => false
+// console.log(null >= 0);  // => true
+
+
+// Comparisons with undefined:
+
+// console.log(undefined); // undefined
+// console.log(undefined == 0); // => false
+// console.log(undefined > 0); // => false
+// console.log(undefined < 0); // => false
+
+// === 
+
+// Avoid these type of comparison
+
+console.log("2" === 2); // => false
