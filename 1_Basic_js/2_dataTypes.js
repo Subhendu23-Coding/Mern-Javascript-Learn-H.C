2873368f74f019b9389638eb88995538b7bd5e2c
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-04-10 06:30:25 +0000

    datatypes

---
+++ 1_Basic_js/2_dataTypes.js
@@ -0,0 +1,43 @@
+"use strict"; // treat all Javascript code as newer version
+
+// alert("hello"); //(we are using nodejs ,)
+// alert(5+5) //(we are using nodejs , not browser)
+
+
+// console.log(5 + 5); // code readability should be high
+// console.log("Subho");
+
+// let name = "Subho"; //string data typ
+// let age = 26
+// console.log(typeof 26 ); // => Number .
+
+let isLoggedIn = false;
+let state ;
+
+// number => 2 to the power 53
+// bigint 
+// string => " "
+// boolean => true/false
+// null => stanalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Subho");
+console.log(typeof undefined); //undefined  
+console.log(typeof null); // object hota hai
+console.log(typeof "w.w");
+
+// undefined
+
+
+/** *
+Explanation
+Boolean: A boolean data type is a single bit of information that can be either true or false. 
+Integer: An integer data type represents whole numbers, and the number of digits can vary based on the device. 
+String: A string data type represents a sequence of characters or text. 
+Array: An array data type is a collection of data items stored in contiguous memory locations. 
+Float: A float data type is a subtype of NUMBER that represents a number value with fractional parts. 
+Char: A char data type stores character data in a fixed-length field. 
+*/
