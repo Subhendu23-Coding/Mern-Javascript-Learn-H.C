3e36874a8bca6aad2309765cf0dded209dd37a76
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-04-10 06:30:55 +0000

    Operations discussed.

---
+++ 1_Basic_js/3_conversionOperation.js
@@ -0,0 +1,76 @@
+// let score = "555";
+// let score = undefined;
+// let score = null;
+// let score = true;
+// let score = "Subho";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let score = "555";
+// let valueInNumber = Number(score); 
+// console.log(typeof valueInNumber);  // "number"
+// console.log(valueInNumber);         // 25
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+
+// let isLoggedIn = 1; // true
+// let isLoggedIn = "name" //true
+// let isLoggedIn = ""; // খালি স্ট্রিং, যা false হবে
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);  // false
+
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "Subho" => true
+
+
+// let someNumber = 42;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);         // "42"
+// console.log(typeof stringNumber);  // "string"
+
+
+// // *** Operations ***
+
+// let value = 5;
+// let negValue = -value;
+// console.log(negValue); // -5
+
+/***************Basic Knowalage */
+// console.log(2+2); //2 + 2 = 4
+// console.log(2-2); //2 - 2 = 0
+// console.log(2*2); //2 * 2 = 4
+// console.log(2**2); //2 ** 2 = 4
+// console.log(2**3); // 2 ** 3 = 8
+// console.log(2/3); //2 / 3 ≈ 0.6666666666666666
+// console.log(2%3); //2 % 3 = 2
+
+
+let str1 = "hello";
+// let str2 = " Subho";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);  // "1" + 2 = 12
+// console.log(1 + "2");  //12
+// console.log("1" + 2 + 2); // "1" + 2 = 12 , 12 + 2 = 122
+// console.log(1 + 2 + "2"); // 1 + 2 = 3 , 3 + 2 = 32
+
+// // console.log((3 + 4) * 5 % 3);
+
+// // num1 = num2 = num3 = 2 + 2;  // very bad practice
+// console.log(+true);  // very bad practice
+// console.log(+""); // 0;
+// console.log(true);
+
+// // let num1, num2, num3;
+
+let gameCounter = 10;
+gameCounter++;
+++gameCounter;             //   gameCounter = 10 gameCounter++ => gameCounter = 11 , 
+console.log(gameCounter); //      gameCounter = 11 gameCounter++ => gameCounter = 12 , 
