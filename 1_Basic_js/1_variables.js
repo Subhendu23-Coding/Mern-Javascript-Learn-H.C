b8170607a10ed2bb3f6cc7f905bc4db793e67f40
Subhendu Majhi <140392393+Subhendu23-Coding@users.noreply.github.com>
2025-04-10 06:20:28 +0000

    discussion on variables

---
+++ 1_Basic_js/1_variables.js
@@ -0,0 +1,21 @@
+const accountId = 234567
+let accountEmail = "subho@.com";
+var accountPassword = "123pcs";
+let accountCity = "Kolkata"; 
+let accountState = "West Bengal";    // all code use this right
+
+// accountId (Only One allowed)
+// accountId = 2; (not allowed)
+
+accountEmail = "google@.com";
+accountPassword = "pcs123";
+accountCity = "Bengaluru";   //..............x.....all this is a bad used
+accountState = "K.T";
+
+// console.log(accountId);
+
+/*
+Prefer not to use (var)
+because of issue in block scope and functional scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
